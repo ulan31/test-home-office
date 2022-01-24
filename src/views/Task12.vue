a9b5cf07282f396183388e4d3d19cde92509a3db
ulan.aliekov <ulan.aliekov@gmail.com>
2022-01-24 08:39:40 +0300

    finish task 1.2

--- src/views/Task12.vue
+++ src/views/Task12.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="container">
     <div class="block-left">
-      <div class="block-greens">
-        <div class="block-greens-row">
-          <div class="block-green"></div>
-          <div class="block-green"></div>
+      <div class="block-tops">
+        <div class="block-tops-row">
+          <div class="block-top"></div>
+          <div class="block-top"></div>
         </div>
-        <div class="block-greens-row">
-          <div class="block-green"></div>
-          <div class="block-green"></div>
+        <div class="block-tops-row">
+          <div class="block-top"></div>
+          <div class="block-top"></div>
         </div>
       </div>
-      <div class="block-whites">
-        <div class="block-white"></div>
-        <div class="block-white"></div>
-        <div class="block-white"></div>
+      <div class="block-bottoms">
+        <div class="block-bottom"></div>
+        <div class="block-bottom"></div>
+        <div class="block-bottom"></div>
       </div>
     </div>
     <div class="block-right">
-      <div class="block-yellow"></div>
-      <div class="block-yellow"></div>
-      <div class="block-yellow"></div>
+      <div class="block-aside"></div>
+      <div class="block-aside"></div>
+      <div class="block-aside"></div>
     </div>
   </div>
 </template>
